7c6b810a84c5b9277eaf471f8905612276564d43
alienfoxx <alienfoxx001@gmail.com>
2024-05-26 11:12:15 +0100

    comeplete the stepe4 of the unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,83 +1,87 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 
-// eslint-disable-next-line react/prop-types
 const Statistics = ({
   good,
   neutral,
   bad,
-  allfeadbacks,
+  allFeedbacks,
   average,
   positive,
 }) => {
   return (
     <div>
       <h1>Statistics</h1>
-      <p>good{good}</p>
-      <p>neutral{neutral}</p>
-      <p>bad{bad}</p>
-      <p>All {allfeadbacks}</p>
-      <p>Average{average}</p>
-      <p>Positive {positive}%</p>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All Feedbacks: {allFeedbacks}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive}%</p>
     </div>
   );
 };
 
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [allfeadbacks, setAllFeadbacks] = useState(0);
+  const [allFeedbacks, setAllFeedbacks] = useState(0);
   const [average, setAverage] = useState(0);
   const [positive, setPositive] = useState(0);
 
-  function handelPositive() {
-    return (good / allfeadbacks) * 100;
+  function handlePositive() {
+    return (good / allFeedbacks) * 100;
   }
 
   return (
     <div>
-      <h1>Give Feedback</h1>
-      <button
-        onClick={() => {
-          setGood(good + 1);
-          setAllFeadbacks(allfeadbacks + 1);
-          setAverage(average + 1);
-          setPositive(handelPositive());
-        }}
-      >
-        Good
-      </button>
-      <button
-        onClick={() => {
-          setNeutral(neutral + 1);
-          setAllFeadbacks(allfeadbacks + 1);
-          setAverage(average);
-          setPositive(handelPositive());
-        }}
-      >
-        neutral
-      </button>
-      <button
-        onClick={() => {
-          setBad(bad + 1);
-          setAllFeadbacks(allfeadbacks + 1);
-          setAverage(average - 1);
-          setPositive(handelPositive());
-        }}
-      >
-        bad
-      </button>
-      <div className="result">
+      <div className="action">
+        <h1>Give Feedback</h1>
+        <button
+          onClick={() => {
+            setGood(good + 1);
+            setAllFeedbacks(allFeedbacks + 1);
+            setAverage(average + 1);
+            setPositive(handlePositive());
+          }}
+        >
+          Good
+        </button>
+        <button
+          onClick={() => {
+            setNeutral(neutral + 1);
+            setAllFeedbacks(allFeedbacks + 1);
+            setAverage(average);
+            setPositive(handlePositive());
+          }}
+        >
+          Neutral
+        </button>
+        <button
+          onClick={() => {
+            setBad(bad + 1);
+            setAllFeedbacks(allFeedbacks + 1);
+            setAverage(average - 1);
+            setPositive(handlePositive());
+          }}
+        >
+          Bad
+        </button>
+      </div>
+
+      {allFeedbacks > 0 ? (
         <Statistics
           good={good}
           neutral={neutral}
           bad={bad}
-          allfeadbacks={allfeadbacks}
+          allFeedbacks={allFeedbacks}
           average={average}
           positive={positive}
         />
-      </div>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   );
 };
