8d9ea28c65f612e1589cfe665e3af80131217142
alienfoxx <alienfoxx001@gmail.com>
2024-05-27 20:29:09 +0100

    ancdote step1

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const [selected, setSelected] = useState(anecdotes[0]);
+
+  const randomquot = () =>
+    anecdotes[Math.floor(Math.random() * anecdotes.length)];
+
+  return (
+    <div>
+      <p>{selected}</p>
+      <button
+        onClick={() => {
+          setSelected(randomquot);
+        }}
+      >
+        Get random anecdote
+      </button>
+    </div>
+  );
+};
+
+export default App;
