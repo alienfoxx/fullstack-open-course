559893731e220b23e2fcfc9a17f84aadad1c8380
alienfoxx <alienfoxx001@gmail.com>
2024-05-27 23:04:42 +0100

    finish the anecdots an part 1

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -4,28 +4,45 @@ const App = () => {
   const anecdotes = [
     "If it hurts, do it more often.",
     "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
     "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
     "Premature optimization is the root of all evil.",
     "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
     "The only way to go fast, is to go well.",
   ];
-  const [selected, setSelected] = useState(anecdotes[0]);
 
-  const randomquot = () =>
-    anecdotes[Math.floor(Math.random() * anecdotes.length)];
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+
+  const getNextAnecdote = () => {
+    const randomAnecdotes = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomAnecdotes);
+  };
+
+  const handleVote = () => {
+    const newVote = [...votes];
+    newVote[selected] += 1;
+    setVotes(newVote);
+  };
+
+  const mostVotes = Math.max(...votes);
+  const bestAnecdote = anecdotes[votes.indexOf(mostVotes)];
 
   return (
     <div>
-      <p>{selected}</p>
-      <button
-        onClick={() => {
-          setSelected(randomquot);
-        }}
-      >
-        Get random anecdote
-      </button>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVote}>Vote</button>
+      <button onClick={getNextAnecdote}>Next Anecdote</button>
+      {mostVotes > 0 && (
+        <>
+          <h1>Anecdote with most votes</h1>
+          <p>{bestAnecdote}</p>
+          <p>has {mostVotes} votes</p>
+        </>
+      )}
     </div>
   );
 };
