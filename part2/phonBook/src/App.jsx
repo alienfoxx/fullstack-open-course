70d2176c08953515eebfd81a7ea888d3e0aa112a
alienfoxx <alienfoxx001@gmail.com>
2025-01-20 12:50:46 +0100

    finshing the phonbook part6

--- part2/phonBook/src/App.jsx
+++ part2/phonBook/src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from "react";
-import Filter from "../components/Filter";
-import PersonFrom from "../components/PersonFrom";
-import Persons from "../components/Persons";
+import { useEffect, useState } from "react";
+import Filter from "./components/Filter";
+import PersonFrom from "./components/PersonFrom";
+import Persons from "./components/Persons";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  const handleEffectHook = () => {
+    try {
+      console.log("effect");
+      axios.get("http://localhost:3001/persons").then((response) => {
+        console.log(" promise fullfild");
+
+        setPersons(response.data);
+      });
+    } catch (error) {
+      console.log("Error while fetching data", error);
+    }
+  };
+
+  useEffect(handleEffectHook, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -40,22 +51,15 @@ const App = () => {
   };
 
   const handleFilterChange = (event) => {
-    const newFilter = event.target.value.trim();
-    setFilter(newFilter);
-
-    if (!newFilter) {
-      console.log("input empty");
-      setPersons(persons);
-
-      return;
-    }
-    const regex = new RegExp(newFilter, "i");
-    const filtredPersone = () =>
-      persons.filter((person) => person.name.match(regex));
-    setPersons(filtredPersone);
-
-    console.log(event.target);
+    setFilter(event.target.value);
   };
+
+  // Filter persons based on the filter value
+  const filteredPersons = filter
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    : persons;
 
   return (
     <div>
@@ -70,7 +74,7 @@ const App = () => {
         handleNumberChange={handleNumberChange}
       />
       <h2>Numbers</h2>
-      <Persons persons={persons} newName={newName} />
+      <Persons persons={filteredPersons} newName={newName} />
       ...
     </div>
   );
